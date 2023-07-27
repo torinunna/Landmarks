22d9c58e8788780806dd93ad894f7684b3581a5f
torinunna <torinunna9588@gmail.com>
2023-07-27 16:38:09 +0900

    Section3. Combine Views Using Stacks

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -11,8 +11,19 @@ import SwiftUI
 //  conform to the View protocol and describe the view’s content and layout
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+                
+            }
+        }
+        .padding()
     }
 }
 

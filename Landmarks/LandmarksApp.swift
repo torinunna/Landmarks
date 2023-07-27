9fefab65e62ddc6425a4e0289bfbb842f23eca36
torinunna <torinunna9588@gmail.com>
2023-07-27 16:22:53 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by YUJIN KWON on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

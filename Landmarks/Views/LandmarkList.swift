622287fe7dd0ffa92e2ad88f161c71f58a4c890d
torinunna <torinunna9588@gmail.com>
2023-07-30 13:31:47 +0900

    Section4. Create the List of Landmarks

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by YUJIN KWON on 2023/07/30.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}

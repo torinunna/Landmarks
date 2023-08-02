8a324033bf6de70a89fa2e71efe48d7c99dadad1
torinunna <torinunna9588@gmail.com>
2023-08-02 15:08:23 +0900

    add a delay to each animation that’s based on the capsule’s position on the graph

--- Landmarks/Views/Hikes/HikeGraph.swift
+++ Landmarks/Views/Hikes/HikeGraph.swift
@@ -11,6 +11,7 @@ extension Animation {
     static func ripple(index: Int) -> Animation {
         Animation.spring(dampingFraction: 0.5)
             .speed(2)
+            .delay(0.03 * Double(index))
     }
 }
 

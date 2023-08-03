5d5ec2ea3ebeb17bb5dd4f450d29636664322127
torinunna <torinunna9588@gmail.com>
2023-08-03 18:08:26 +0900

    Section2. Add an Edit Mode

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
+            
         }
         .padding()
     }
@@ -21,5 +34,6 @@ struct ProfileHost: View {
 struct Profiles_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
